fda55ddb33213bedb1ada5ad699a1123f059f244
Janko Hu <hu.janko@yahoo.de>
2023-11-27 20:23:13 +0100

    fixed: run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,17 @@
-make
-./TextualTide
+#!/bin/bash
+
+# Build the tests and the application
+make test
+make run
+
+# Run the tests
+echo "Running tests..."
+./TextualTideTests
+
+# If tests are successful, run the main application
+if [ $? -eq 0 ]; then
+    echo "Running main application..."
+    ./TextualTide
+else
+    echo "Tests failed. Not running main application."
+fi
